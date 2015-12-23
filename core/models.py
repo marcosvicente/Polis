d13669bebcc9dbda9f2ba7976e58d42927af9045
Marcos Vicente <marcosvicentesiqueira@gmail.com>
2015-12-23 00:39:13 -0200

    indentacao

--- core/models.py
+++ core/models.py
@@ -1,3 +1,35 @@
+# _*_ coding: utf-8 _*_
+
 from django.db import models
 
-# Create your models here.
+
+class PostEventos(models.Model):
+    nome = models.CharField(max_length=50)
+    estado = models.CharField(max_length=50)
+    cidade = models.CharField(max_length=50)
+    data = models.DateField()
+    hora = models.DateTimeField()
+    url = models.URLField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class PostTexto(models.Model):
+    titulo = models.CharField(max_length=50, null=True)
+    texto = models.TextField(null=True)
+    data = models.DateField()
+
+
+def __str__(self):
+    return self.titulo
+
+
+class PostImage(models.Model):
+    image = models.ImageField()
+    data = models.DateField()
+
+
+class PostVideo(models.Model):
+    video = models.FileField()
+    data = models.DateField()
--- tcc/settings.py
+++ tcc/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -79,15 +80,9 @@ WSGI_APPLICATION = 'tcc.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'mydatabase',
     }
 }
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.8/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
